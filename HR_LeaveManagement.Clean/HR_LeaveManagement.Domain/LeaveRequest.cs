48b6409c8ee46647bf14f1056df67c90f70e9e04
Damon Jacquemin <damon.jacquemin@gmail.com>
2024-06-18 16:09:15 +0200

    InitProject + Domain class library

---
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.Domain/LeaveRequest.cs
@@ -0,0 +1,23 @@
+﻿using HR_LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Domain;
+
+public class LeaveRequest : BaseEntity
+{
+    public DateTime StartingDate { get; set; }
+    public DateTime EndingDate { get; set; }
+    public LeaveType? LeaveType { get; set; }
+    public int LeaveTypeId { get; set; }
+    public DateTime RequestedDate { get; set; }
+    public string? RequestComments { get; set; }
+    public bool? Approved { get; set; }
+    public bool Cancelled { get; set; }
+
+    public string RequestingEmployeeId { get; set; } = string.Empty;
+}
---
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.Domain/LeaveType.cs
@@ -0,0 +1,14 @@
+﻿using HR_LeaveManagement.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Domain;
+
+public class LeaveType : BaseEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public int DefaultDays { get; set; }
+}
