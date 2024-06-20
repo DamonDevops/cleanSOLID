ef408be8e0d1fb225361171d0bcc203ca8184ec2
Damon Jacquemin <damon.jacquemin@gmail.com>
2024-06-20 16:55:38 +0200

    Add Task to validate unicity of LeaveType in repository

--- HR_LeaveManagement.Clean/HR_LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.Application/Contracts/Persistence/ILeaveTypeRepository.cs
@@ -4,5 +4,5 @@ namespace HR_LeaveManagement.Application.Contracts.Persistence;
 
 public interface ILeaveTypeRepository : IGenericRepository<LeaveType>
 {
-
+    Task<bool> IsLeaveTypeUnique(string name);
 }
--- HR_LeaveManagement.Clean/HR_LeaveManagement.Application/Exceptions/NotFoundException.cs
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.Application/Exceptions/NotFoundException.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace HR_LeaveManagement.Application.Exceptions
 {
-    internal class NotFoundException
+    public class NotFoundException : Exception
     {
+        public NotFoundException(string name, object key) : base($"{name} ({key}) was not found")
+        {
+
+        }
     }
 }
