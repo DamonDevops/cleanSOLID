aafa2995efebaf58db7b299390abcbfe34415042
Damon Jacquemin <damon.jacquemin@gmail.com>
2024-06-21 15:12:56 +0200

    Add EntityFramework.sqlserver & ConfigurationExtension
    create service registration class

---
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace HR_LeaveManagement.Persistence;
+
+public static class PersistenceServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+
+        return services;
+    }
+}
