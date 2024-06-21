7873bc84ec9307a86dbd38038d84e2e1c6e5a448
Damon Jacquemin <damon.jacquemin@gmail.com>
2024-06-21 15:42:51 +0200

    Setting EntityFramework and Dbcontext
    + adding related services

--- HR_LeaveManagement.Clean/HR_LeaveManagement.Persistence/PersistenceServiceRegistration.cs
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.Persistence/PersistenceServiceRegistration.cs
@@ -1,12 +1,17 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using HR_LeaveManagement.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HR_LeaveManagement.Persistence;
 
 public static class PersistenceServiceRegistration
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration config)
     {
-
+        services.AddDbContext<HrDbContext>(options => {
+            options.UseSqlServer(config.GetConnectionString("HrDatabaseString"));
+        });
         return services;
     }
 }
