e371ab5742859ccf5b324ac898dc76f12317f369
Damon Jacquemin <damon.jacquemin@gmail.com>
2024-07-01 08:58:27 +0200

    adding Controller with mediatR, response code, corrective on validators
    unique leave type was sending "false" instead of "true", Add mapping in MappingProfile

---
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.API/Controllers/LeaveTypeController.cs
@@ -0,0 +1,69 @@
+﻿using HR_LeaveManagement.Application.Features.LeaveType.Commands.CreateLeaveType;
+using HR_LeaveManagement.Application.Features.LeaveType.Commands.DeleteLeaveType;
+using HR_LeaveManagement.Application.Features.LeaveType.Commands.UpdateLeaveType;
+using HR_LeaveManagement.Application.Features.LeaveType.Queries.GetAllLeaveTypes;
+using HR_LeaveManagement.Application.Features.LeaveType.Queries.GetLeaveTypeDetails;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR_LeaveManagement.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LeaveTypeController : ControllerBase
+{
+    public IMediator _mediator;
+
+    public LeaveTypeController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet]
+    public async Task<List<LeaveTypeDTO>> Get()
+    {
+        var leaveTypes = await _mediator.Send(new GetLeaveTypesQuery());
+        return leaveTypes;
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<LeaveTypeDetailDTO>> GetById(int id)
+    {
+        return Ok(await _mediator.Send(new GetLeaveTypeDetailsQuery(id)));
+    }
+
+    [HttpPost]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    public async Task<ActionResult> Post([FromBody] CreateLeaveTypeCommand createLeaveTypeCommand) 
+    {
+        var response = await _mediator.Send(createLeaveTypeCommand);
+        return CreatedAtAction(nameof(Get), new { id = response });
+    }
+
+    [HttpPut("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> Put(UpdateLeaveTypeCommand updateLeaveTypeCommand)
+    {
+        await _mediator.Send(updateLeaveTypeCommand);
+        return NoContent();
+    }
+
+    // DELETE api/<LeaveTypeController>/5
+    [HttpDelete("{id}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    [ProducesDefaultResponseType]
+    public async Task<ActionResult> Delete(int id)
+    {
+        var command = new DeleteLeaveTypeCommand { Id = id };
+        await _mediator.Send(command);
+        return NoContent();
+    }
+}
