d101ce05f2ae12b7206773c5b0f91a4f0fe08dc7
Damon Jacquemin <damon.jacquemin@gmail.com>
2024-07-05 16:50:17 +0200

    Preparing Identification with services

---
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.Application/Contracts/Identity/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Contracts.Identity;
+
+public interface IUserService
+{
+    Task<List<Employee>> GetEmployees();
+    Task<Employee> GetEmployeeById(string userId);
+}
---
+++ HR_LeaveManagement.Clean/HR_LeaveManagement.Application/Models/IdentityModels/RegistrationRequest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR_LeaveManagement.Application.Models.IdentityModels;
+
+public class RegistrationRequest
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    [MinLength(8)]
+    public string Password { get; set; }
+}
